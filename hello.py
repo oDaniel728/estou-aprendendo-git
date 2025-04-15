10d23a433dede0a2ce09c7d64b16a48602bb9e02
oDaniel728 <danielmenezes0822@gmail.com>
2025-04-14 22:09:23 -0300

    simple hello world script update

--- hello.py
+++ hello.py
@@ -1,1 +1,20 @@
-print("hello world!")
+from typing import List
+import sys
+
+class Main:
+    def main(self, args: List[str]) -> None:
+        self.print()
+        
+    def print(self) -> None:
+        sys.stdout.write("Hello, World!\n")
+
+def main(sys):
+    if __name__ == "__main__":
+        try:
+            Main().main(sys.argv)
+            return 0
+        except Exception as e:
+            sys.stderr.write("Error (" + str(e.__cause__.__class__.__name__) + ") => " + str(e))
+            return 1
+  
+sys.stdout.write(main(sys) + "\n")
